60be1c708b62058f481379e5e4dfdee6dbe24c3f
hybo <52079867+me1ting@users.noreply.github.com>
2021-12-27 10:40:26 +0800

    完善商品测试程序

--- trade-test.js
+++ trade-test.js
@@ -1,6 +1,42 @@
 import { translateGoods } from "./trade.js";
+import {
+    open,
+    readdir
+} from 'node:fs/promises';
 
-let goods = "物品类别: 箭袋\r\n稀 有 度: 稀有\r\n咒缚 掌爪\r\n刺锋箭袋\r\n--------\r\n需求:\r\n等级: 64\r\n--------\r\n物品等级: 84\r\n--------\r\n攻击和法术暴击率提高 27% (implicit)\r\n--------\r\n攻击速度提高 6%\r\n弓类攻击的暴击率提高 11%\r\n弓类攻击 +27% 暴击伤害加成\r\n+39 最大生命\r\n弓类攻击发射一支额外箭矢\r\n攻击技能的元素伤害提高 30% (crafted)\r\n--------\r\n督军物品\r\n--------\r\n出售获得通货: 非绑定\r\n";
+const testFolder = './testcases/goods';
+const testcasesSuffix = ".txt";
+const resSuffix = ".transalted.txt";
 
-let result = translateGoods(goods);
-console.log(result);
+const test = async (file) => {
+    let readFileHandle;
+    let writeFileHandle;
+    let path = `${testFolder}/${file}`;
+
+    try {
+        readFileHandle = await open(path);
+        const content = await readFileHandle.readFile();
+        let goods = content.toString();
+        if (!content.includes("\r\n")) {
+            goods = goods.replaceAll("\n", "\r\n");
+        }
+
+        let res = translateGoods(goods);
+        writeFileHandle = await open(`${path}${resSuffix}`, "w+");
+        await writeFileHandle.writeFile(res);
+    } finally {
+        await writeFileHandle?.close();
+        await readFileHandle?.close();
+    }
+}
+
+try {
+    const files = await readdir(testFolder);
+    for (const file of files) {
+        if (file.endsWith(testcasesSuffix)&&!file.endsWith(resSuffix)) {
+            test(file);
+        }
+    }
+} catch (err) {
+    console.error(err);
+}
